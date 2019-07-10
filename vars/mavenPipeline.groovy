b4a3a970c1ed9d0e16c45e2292d9a729c158c78e
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-07-10 18:57:10 +0530

    Update mavenPipeline.groovy

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -1,24 +1,20 @@
-def label = "docker-slave-${UUID.randomUUID().toString()}"
-podTemplate(label: label, containers: [
-    containerTemplate(name: 'docker', image: 'durgaprasad444/jenmine:1.1', ttyEnabled: true, command: 'cat')
-],
-volumes: [
-  hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')
-]) {
-    node(label) {
-        def APP_NAME = "hello-world"
-        def tag = "dev"
-            stage("clone code") {
-                container('slave1') {
-                    
-                    // Let's clone the source
-                    sh """ 
-                      git clone https://github.com/durgaprasad444/${APP_NAME}.git            
-                      cd ${APP_NAME}
-                      cp -r * /home/jenkins/workspace/maven-example
-                    """
-                }
-            }
+def call() {
+  node {
+    stage('Checkout') {
+      checkout scm
+    }
+    def p = pipelineCfg()
 
-                }
-            }
+    if (p.runTests == true) {
+      docker.image(p.testImage).inside() {
+        stage('Test') {
+          sh 'ls'
+          
+          //sh p.testCommand
+        }
+      }
+    }
+
+  
+  }
+}
